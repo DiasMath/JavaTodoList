834387d797f2e9ba2904e1da04872c1ae8252b17
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-10-14 04:07:56 -0300

    Aula 02

--- src/main/java/br/com/matheusdias/todolist/user/UserController.java
+++ src/main/java/br/com/matheusdias/todolist/user/UserController.java
@@ -1,18 +1,34 @@
 package br.com.matheusdias.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.experimental.var;
 
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario existente");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
