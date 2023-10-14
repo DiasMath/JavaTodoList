bbd3f2396397042eee505fbd03db3a89d64607fc
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-10-14 14:03:57 -0300

    adicionando criptografica para a password

--- src/main/java/br/com/matheusdias/todolist/user/UserController.java
+++ src/main/java/br/com/matheusdias/todolist/user/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import lombok.experimental.var;
 
 
@@ -26,6 +27,10 @@ public class UserController {
         if (user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario existente");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
